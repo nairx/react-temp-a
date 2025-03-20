3e07c350bade5f4dabea558ce3e2021014f002af
nairx <nairxprv@gmail.com>
2025-03-20 12:21:31 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { useState } from "react";
-import "./App.css";
+// import "./App.css";
 import { BrowserRouter, Link,Routes,Route } from "react-router-dom";
 import App1 from "./components/App1";
 import App2 from "./components/App2";
+import App3 from "./components/App3";
+import App4 from "./components/App4";
 function App() {
   return (
     <BrowserRouter>
       <div>
         <Link to="app1">App1</Link>
         <Link to="app2">App2</Link>
+        <Link to="app3">App3</Link>
+        <Link to="app4">App4</Link>
       </div>
       <hr></hr>
       <div>
@@ -16,6 +20,8 @@ function App() {
           <Route index element={<App1 />} />
           <Route path="app1" element={<App1 />} />
           <Route path="app2" element={<App2 />} />
+          <Route path="app3" element={<App3 />} />
+          <Route path="app4" element={<App4 />} />
         </Routes>
       </div>
     </BrowserRouter>
--- src/components/App1.jsx
+++ src/components/App1.jsx
@@ -1,3 +1,8 @@
 export default function App1() {
-  return <h1>This is App1</h1>;
+  return (
+    <div>
+      <h3>This is App1</h3>
+      <p style={{backgroundColor:'pink',color:'blue'}}>This is a paragraph</p>
+    </div>
+  );
 }
