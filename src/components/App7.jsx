5c33031cad83924aedffe5e06f628d9884a0d6fb
nairx <nairxprv@gmail.com>
2025-03-25 14:02:11 +0530

    Updated

---
+++ src/components/App7.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { useState, useEffect } from "react";
+export default function App7() {
+  const [a, setA] = useState(0);
+  const [b, setB] = useState(0);
+  const [result, setResult] = useState(0);
+  return (
+    <div>
+      <h3>This is App7</h3>
+      <p>
+        <input type="number" onChange={(e)=>setA(e.target.value)}></input>
+      </p>
+      <p>
+        <input type="number" onChange={(e)=>setB(e.target.value)}></input>
+      </p>
+      <button>Submit</button>
+      <p>
+        {result}
+      </p>
+    </div>
+  );
+}
