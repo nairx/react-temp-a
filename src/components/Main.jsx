17937b013edd408eebb41300b012f647d7a43940
nairx <nairxprv@gmail.com>
2025-03-27 12:31:02 +0530

    Updated

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import Child1 from "./Child1";
 import Child2 from "./Child2";
 export default function Main(props) {
-  if (props.c === "a") {
-    return <Child1 />;
-  } else {
-    return <Child2 />;
+  {
+    return props.c === "a" ? <Child1 /> : <Child2 />;
   }
+  //   if (props.c === "a") {
+  //     return <Child1 />;
+  //   } else {
+  //     return <Child2 />;
+  //   }
 }
