be9d7b911956a2387b6ed91d2acf0c2481ff998e
nairx <nairxprv@gmail.com>
2025-04-03 14:30:03 +0530

    Updated

--- src/components/App18.jsx
+++ src/components/App18.jsx
@@ -1,16 +1,8 @@
 import React from "react";
 import { useMemo, useState } from "react";
 export default function App18() {
-  const [length, setLength] = useState(1000);
+  const [length, setLength] = useState(100);
   const [count, setCount] = useState(0);
-  //   const [result, setResult] = useState(0);
-
-  //   const f1 = useMemo(() => {
-  //     let i;
-  //     for (i = 0; i < length ** 2; i++) {}
-  //     console.log("f1 function called");
-  //     return i;
-  //   }, [length]);
 
   const f1 = () => {
     let i;
@@ -20,7 +12,7 @@ export default function App18() {
   };
 
   const result = useMemo(() => f1(), [length]);
-  //   const result = f1()
+    // const result = f1()
 
   return (
     <div>
--- src/components/Child.jsx
+++ src/components/Child.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import { memo } from "react";
 function Child(props) {
-  console.log("Child Component Loaded",props.hrs);
+  console.log("Child Component Loaded", props.hrs);
   {
-    return props.hrs < 38 ? <h4>Good Morning</h4> : <h4>Good Afternoon</h4>;
+    return props.hrs < 21 ? <h4>Good Morning</h4> : <h4>Good Afternoon</h4>;
   }
 }
 
-export default memo(Child)
-
+export default memo(Child);
